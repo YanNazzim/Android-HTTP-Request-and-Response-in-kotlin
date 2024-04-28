338c1d7018b8c6e7341030a99c4dcb1116544ca8
Yan <ygonz42@gmail.com>
2024-04-27 20:58:31 -0400

    Lab Complete

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -8,25 +8,46 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val responseLiveData = MutableLiveData<String>()
+
+    private val httpClient = HttpClient(Android)
+
+    private suspend fun fetchContent(): String {
+        return httpClient
+            .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonMenu.json")
+            .bodyAsText()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LittleLemonTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val responseState = responseLiveData.observeAsState("").value
+
                     Column {
                         Button(
                             onClick = {
-
+                                lifecycleScope.launch{val response = fetchContent()
+                                    runOnUiThread{responseLiveData.value= response}}
                             }
                         ) {
                             Text(text = "Download")
                         }
+                        Text(text = responseState.toString())
                     }
                 }
             }
